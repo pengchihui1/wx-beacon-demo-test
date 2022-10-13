073ad3cf1482358db760d79dae562c3e2877397a
pengchihui1 <pengchihui1@gmail.com>
2022-10-13 16:21:02 +0800

    调整为五个区域图

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const brtloc = require('./libs/brtloc.miniprogram.js');
 
 App({
     // // 示例建筑ID
-    // buildingID: 'ZS020227',
+    buildingID: 'ZS020227',
     // // 建筑有效token
-    // token: '5d4dfa4205ba4e2283e782780e091236',
+    token: '5d4dfa4205ba4e2283e782780e091236',
     // 示例建筑ID
-    buildingID: '00230083',
+    // buildingID: '00230083',
     // 建筑有效token
-    token: '9846c87aab7848679b9c001415f1d6be',
+    // token: '9846c87aab7848679b9c001415f1d6be',
     // 唯一ID （建议使用openId）  - RandomUserId：随机分配唯一ID
     userId: brtloc.RandomUserId(),
     // 定位对象
@@ -58,6 +58,7 @@ App({
                   success: function (res) {//蓝牙状态：打开
                     wx.startBeaconDiscovery({//开始搜索附近的iBeacon设备
                       uuids: ['e2c56db5-dffb-48d2-b060-d0f5a71096e0'],//参数uuid
+                      // uuids:['fda50693-a4e2-4fb1-afcf-c6eb07647825'],
                       success: function (res) {
                         wx.onBeaconUpdate(function (res) {//监听 iBeacon 设备的更新事件  
                           console.log("掃描的設備",res.beacons)
